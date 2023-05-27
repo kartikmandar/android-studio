e3fdce1d959c2c869da3a1a37ae941b1f6697d9f
kartikmandar <kartik4321mandar@gmail.com>
2023-05-27 14:39:58 +0530

    Add images in double dice roller app

--- DoubleDiceRoll/app/src/main/java/com/example/doublediceroll/MainActivity.kt
+++ DoubleDiceRoll/app/src/main/java/com/example/doublediceroll/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.doublediceroll
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import org.w3c.dom.Text
 
@@ -14,6 +15,9 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.button)
         //Set a click listener on the roll button to roll the dice when the user clicks the button
         rollButton.setOnClickListener { rollDice() }
+
+        //Do a dice roll when app starts
+        rollDice()
     }
 
     private fun rollDice() {
@@ -29,6 +33,34 @@ class MainActivity : AppCompatActivity() {
         val resultTextView2: TextView = findViewById(R.id.textView3)
         resultTextView2.text = diceRoll2.toString()
 
+        //Find the image in the layout
+        val diceImage1: ImageView = findViewById(R.id.imageView)
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
+
+        //Determine which drawable resource to use on dice roll
+        val drawableResource1 = when (diceRoll1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val drawableResource2 = when (diceRoll2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        //Update the ImageView with the correct drawable resource ID
+        diceImage1.setImageResource(drawableResource1)
+        diceImage2.setImageResource(drawableResource2)
+
+        //Update the content description
+        diceImage1.contentDescription = diceRoll1.toString()
+        diceImage2.contentDescription = diceRoll2.toString()
     }
 }
 

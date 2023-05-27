31f70ac0160cceee15d1e921d372389b2065923e
kartikmandar <kartik4321mandar@gmail.com>
2023-05-27 14:18:57 +0530

    Add image of dice on roll

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -15,6 +16,9 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
 
+        //Do a dice roll when the app starts
+        rollDice()
+
     }
 
     /**
@@ -24,9 +28,25 @@ class MainActivity : AppCompatActivity() {
         //Create a new dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        //Update the screen with the result of dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+
+        //Find the imageView in the layout
+        val diceImage: ImageView = findViewById(R.id.imageView2)
+
+        //Determine which drawable resource ID to use based on the dice roll
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        //Update the imageView with the correct drawable resource id
+        diceImage.setImageResource(drawableResource)
+
+        //Update the content Description
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
